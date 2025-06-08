23379d7707f22824eb2d6df8ddc56db867ce23dc
user <foysalhaque.mist.ipe@gmail.com>
2025-06-08 21:23:27 +0600

    first commit

---
+++ src/Router/Route.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router";
+import Home from "../Pages/Home";
+
+export const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Home></Home>,
+children:[
+    {
+
+    }
+]
+    }
+])
