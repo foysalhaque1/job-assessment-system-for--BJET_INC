c321dad9399a127e11ecd045cd5287a5b8ec67f2
user <foysalhaque.mist.ipe@gmail.com>
2025-06-17 10:26:59 +0600

    update

--- src/Pages/Banner/Faq2.jsx
+++ src/Pages/Banner/Faq2.jsx
@@ -7,28 +7,28 @@ const Faq2 = () => {
        <div className='w-11/12 mx-auto bg-amber-100 p-5'>
             <h2 className='text-2xl font-bold text-center'>FAQ Section</h2>
              <div className='max-w-6xl mx-auto border m-8 p-7 rounded-2xl'>
-                        <h1>Question:How can i create assignment? </h1>
-                        <h2>Answer:In the profile picture by clicking this anyone can find the create assignment feature </h2>
+                        <h1 className='text-blue-600 font-bold'>Question:How can i create assignment? </h1>
+                        <h2 className='font-bold text-black'>Answer:In the profile picture by clicking this anyone can find the create assignment feature </h2>
 
                     </div>
              <div className='max-w-6xl mx-auto border m-8 p-7 rounded-2xl'>
-                        <h1>Question:How can i submit assignment? </h1>
-                        <h2>Answer:If anyone sign in user can see the assignment details by clicking see more button where take take assignment button is remaining. User can submit assignment by clicking take assignment button. </h2>
+                        <h1 className='text-blue-600 font-bold'>Question:How can i submit assignment? </h1>
+                        <h2 className='font-bold text-black'>Answer:If anyone sign in user can see the assignment details by clicking see more button where take take assignment button is remaining. User can submit assignment by clicking take assignment button. </h2>
 
                     </div>
              <div className='max-w-6xl mx-auto border m-8 p-7 rounded-2xl'>
-                        <h1>Question:Can any one submit assignment? </h1>
-                        <h2>Answer:Yes the user who is ssign in can submit assignment. </h2>
+                        <h1 className='text-blue-600 font-bold'>Question:Can any one submit assignment? </h1>
+                        <h2 className='font-bold text-black'>Answer:Yes the user who is ssign in can submit assignment. </h2>
 
                     </div>
              <div className='max-w-6xl mx-auto border m-8 p-7 rounded-2xl'>
-                        <h1>Question:How can i get marks? </h1>
-                        <h2>Answer:The other user who didn't create the assignment can give marks by give marks button </h2>
+                        <h1 className='text-blue-600 font-bold'>Question:How can i get marks? </h1>
+                        <h2 className='font-bold text-black'>Answer:The other user who didn't create the assignment can give marks by give marks button </h2>
 
                     </div>
              <div className='max-w-6xl mx-auto border m-8 p-7 rounded-2xl'>
-                        <h1>Question:How can get the attempted assignment which has submiited by myself? </h1>
-                        <h2>Answer:By clicking the profile picture the attepted assignment button shows.After clicking the button the user can see the attempted assignment which has been submitted by him/herself. </h2>
+                        <h1 className='text-blue-600 font-bold'>Question:How can get the attempted assignment which has submiited by myself? </h1>
+                        <h2 className='font-bold text-black'>Answer:By clicking the profile picture the attepted assignment button shows.After clicking the button the user can see the attempted assignment which has been submitted by him/herself. </h2>
 
                     </div>
         </div>
--- src/Pages/Banner/FeatureSection.jsx
+++ src/Pages/Banner/FeatureSection.jsx
@@ -5,7 +5,7 @@ const FeatureSection = () => {
        <div className='mt-20 text-center mb-16 bg-amber-100 p-8 ' >
 
             <h2 className='text-5xl font-bold ' >Feature of this application</h2>
-            <p className='mt-10 text-2xl mb-10'> This typically involves a user interface where instructors can input details like title, instructions, due date, and points, and optionally attach files, rubrics, or other resources. Students should then be able to access and submit their work through a similar interface</p>
+            <p className='mt-10 text-2xl mb-10 font-bold text-black'> This typically involves a user interface where instructors can input details like title, instructions, due date, and points, and optionally attach files, rubrics, or other resources. Students should then be able to access and submit their work through a similar interface</p>
         
             <div className='grid grid-cols-1'>
                
@@ -13,32 +13,32 @@ const FeatureSection = () => {
                     <div className='border-2 p-3 '>
 
                         <h2 className='text-2xl text-blue-700 font-bold'>Assignment List</h2>
-                        <p>a feature to display a list of assignments is commonly included <br /> in assignment websites. This list typically shows upcoming, past due, and completed assignments, often with details like deadlines and submission status. It helps users stay organized and manage their workload effectively. </p>
+                        <p className='font-bold text-black'>a feature to display a list of assignments is commonly included <br /> in assignment websites. This list typically shows upcoming, past due, and completed assignments, often with details like deadlines and submission status. It helps users stay organized and manage their workload effectively. </p>
                     </div>
                     <div className='border-2 p-3 '>
 
                         <h2 className='text-2xl text-blue-700 font-bold'>Create Assignment</h2>
-                        <p>This functionality is typically found within the instructor or teacher's interface, allowing them to define assignment details, instructions, due dates, and point values.</p>
+                        <p className='font-bold text-black'>This functionality is typically found within the instructor or teacher's interface, allowing them to define assignment details, instructions, due dates, and point values.</p>
                     </div>
                     <div className='border-2 p-3 '>
 
                         <h2 className='text-2xl text-blue-700 font-bold'>My Attempted Assignment</h2>
-                        <p>  Include a "My Attempted Assignments" feature. This feature typically allows students to view, manage, and track their assigned tasks within the platform.</p>
+                        <p className='font-bold text-black'>  Include a "My Attempted Assignments" feature. This feature typically allows students to view, manage, and track their assigned tasks within the platform.</p>
                     </div>
                     <div className='border-2 p-3 '>
 
                         <h2 className='text-2xl text-blue-700 font-bold'>Assignment Details Page</h2>
-                        <p>  This feature typically provides students with comprehensive information about each assignment, including instructions, due dates, points possible, and any relevant resources. .</p>
+                        <p className='font-bold text-black'>  This feature typically provides students with comprehensive information about each assignment, including instructions, due dates, points possible, and any relevant resources. .</p>
                     </div>
                     <div className='border-2 p-3 '>
 
                         <h2 className='text-2xl text-blue-700 font-bold'>Give Mark Page</h2>
-                        <p>  This feature typically provides other users can observe the assignment and can provide feedback,also can give marks .</p>
+                        <p className='font-bold text-black'>  This feature typically provides other users can observe the assignment and can provide feedback,also can give marks .</p>
                     </div>
                     <div className='border-2 p-3 '>
 
                         <h2 className='text-2xl text-blue-700 font-bold'>Pending Assignment Page</h2>
-                        <p>  This feature typically provides the assignments which marks not included</p>
+                        <p className='font-bold text-black'>  This feature typically provides the assignments which marks not included</p>
                     </div>
                 </div>
             </div>
